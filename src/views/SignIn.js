267e2e171e21fa7d605c476bb7981da0ce058862
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2021-10-19 10:02:13 -0500

    removed reactstrap because of deploy errors

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Spinner } from 'reactstrap';
 import { signInUser } from '../api/auth';
 
 const WelcomeStyle = styled.h1`
@@ -12,12 +11,7 @@ export default function SignIn({ user }) {
   return (
     <>
       {user === null ? (
-        <div className="text-center">
-          <Spinner
-            style={{ width: '10rem', height: '10rem' }}
-            color="warning"
-          />
-        </div>
+        <WelcomeStyle>Loading...</WelcomeStyle>
       ) : (
         <div className="text-center mt-5">
           <WelcomeStyle>Welcome! Sign In!</WelcomeStyle>
